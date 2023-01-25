265c1c3eb085d203dab0cd1a9e404902f92ba0a8
Tate <tate@cloudcannon.com>
2023-01-26 11:13:17 +1300

    More typechecking

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
+const seoSchema = z
+  .object({
+    page_description: z.string().nullable(),
+    canonical_url: z.string().nullable(),
+    featured_image: z.string().nullable(),
+    featured_image_alt: z.string().nullable(),
+    author_twitter_handle: z.string().nullable(),
+    open_graph_type: z.string().nullable(),
+    no_index: z.boolean(),
+  })
+  .optional();
+
 const blogCollection = defineCollection({
   schema: z.object({
     date: z.date(),
@@ -14,18 +26,33 @@ const blogCollection = defineCollection({
       image: z.string(),
       image_alt: z.string(),
     }),
-    seo: z.object({
-      page_description: z.string().nullable(),
-      canonical_url: z.string().nullable(),
-      featured_image: z.string(),
-      featured_image_alt: z.string().nullable(),
-      author_twitter_handle: z.string().nullable(),
-      open_graph_type: z.string(),
-      no_index: z.boolean(),
-    }),
+    seo: seoSchema,
   }),
+});
+
+const pageSchema = z.object({
+  _schema: z.any().optional(),
+  title: z.string(),
+  content_blocks: z.array(z.any()),
+  page_size: z.undefined(),
+  description: z.undefined(),
+  seo: seoSchema,
+});
+
+const paginatedCollectionSchema = z.object({
+  _schema: z.literal("paginated_collection"),
+  title: z.string(),
+  description: z.string().optional(),
+  page_size: z.number().positive(),
+  content_blocks: z.undefined(),
+  seo: seoSchema,
+});
+
+const pagesCollection = defineCollection({
+  schema: z.union([paginatedCollectionSchema, pageSchema]),
 });
 
 export const collections = {
   blog: blogCollection,
+  pages: pagesCollection,
 };
--- src/content/pages/contact.md
+++ src/content/pages/contact.md
@@ -4,6 +4,7 @@ seo:
   page_description:
   canonical_url:
   featured_image:
+  featured_image_alt:
   author_twitter_handle:
   open_graph_type:
   no_index: false
