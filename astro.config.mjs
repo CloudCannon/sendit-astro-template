518edcd8901f69ae496771a207847771a79162ad
Tate <tate@cloudcannon.com>
2023-01-25 15:27:35 +1300

    Example baseUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com/",
+  site: "https://top-quail.cloudvent.net/",
   integrations: [react()]
 });
