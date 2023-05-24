ab24d6ff2768ee8f0fd9efa4e3c5107e0ff8137b
Tate <tate@cloudcannon.com>
2023-05-24 12:55:43 +1200

    Use nested bookshop folders

--- src/shared/astro/page.jsx
+++ src/shared/astro/page.jsx
@@ -1,13 +1,21 @@
-const components = import.meta.glob("../../components/**/*.jsx", {
+const components = {};
+const componentImports = import.meta.glob("../../components/**/*.jsx", {
   eager: true,
+});
+Object.entries(componentImports).forEach(([path, obj]) => {
+  const parts = path.replace("../../components/", "").split(".")[0].split("/");
+  if (parts.length > 1 && parts[parts.length - 1] === parts[parts.length - 2]) {
+    parts.pop();
+  }
+  const bookshopName = parts.join("/");
+  components[bookshopName] = obj.default;
 });
 
 export default function Page({ contentBlocks }) {
   return (
     <main>
       {contentBlocks.map((block, i) => {
-        const name = `../../components/${block._bookshop_name}.jsx`;
-        const Component = components[name].default;
+        const Component = components[block._bookshop_name];
         return <Component {...block} key={i} />;
       })}
     </main>
